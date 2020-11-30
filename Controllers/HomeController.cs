f135122f74a47b3b4f335c92e0f2a3b6540671ce
Bekzat Karayev <71756405+bekzatkarayev@users.noreply.github.com>
2020-11-30 14:36:33 +0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using FirstProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+//using System.Web.Mvc;
+
+namespace FirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("MyView");
+        }
+
+        public ViewResult Privacy()
+        {
+            return View("Privacy");
+        }
+    }
+}
